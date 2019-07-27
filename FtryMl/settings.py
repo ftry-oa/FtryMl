0fd87d39b1cc8fb78fe82a350956c52c5cef122f
suifengfengye <1527261080@qq.com>
2019-07-27 15:50:20 +0800

    add allowed hosts

--- FtryMl/settings.py
+++ FtryMl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%nf=50wxqe18(is4x)-*iyax0r)^imszd1rjx*pk#n-wl+hl4('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
